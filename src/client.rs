7c2f3a994b665c59d678ac812937ec4ac1cf26e5
Mayo <mayo@Mayos-MacBook-Pro.local>
2023-04-30 10:51:07 -0500

    refactor client

--- src/client.rs
+++ src/client.rs
@@ -1,57 +1,131 @@
 use std::{convert::TryInto, slice::from_mut};
 
 use std::sync::{Arc, Mutex};
+use cpal::StreamConfig;
 use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
 use bytes::Bytes;
+use futures::stream::{SplitSink, SplitStream};
 use serde::Serialize;
 use tokio::net::TcpStream;
-use tokio_tungstenite::connect_async;
+use tokio_tungstenite::tungstenite::handshake::client::Response;
+use tokio_tungstenite::{connect_async, WebSocketStream, MaybeTlsStream};
 use tokio_tungstenite::{accept_async, tungstenite::Message};
 use futures::sink::Send;
 
 
 use futures::{StreamExt, SinkExt};
 
+struct Connection {
+    url: String,
+    writer: SplitSink<WebSocketStream<MaybeTlsStream<TcpStream>>, Message>,
+    reader: SplitStream<WebSocketStream<MaybeTlsStream<TcpStream>>>,
+    config: cpal::SupportedStreamConfig
+}
+
+impl Connection {
+    async fn connect_to_server(url: &str, config: cpal::SupportedStreamConfig) -> Result<(Connection, Response), Box<dyn std::error::Error>> {
+        let (socket, resp) = connect_async(url).await?;
+        let (writer, reader) = socket.split();
+        
+        let connection = Connection {
+         url: url.to_owned(),
+         writer,
+         reader,
+         config
+        };
+     
+        return Ok((connection, resp));
+     }
+
+     async fn send_client_config(connection: &mut Connection) -> Result<(), Box<dyn std::error::Error>> {
+        let bin_config_struct = remoteio::BinStreamConfig {
+            channels:  connection.config.channels(),
+            sample_rate: connection.config.sample_rate().0,
+            buffer_size: 4096
+        };
+    
+        let bin_config = bincode::serialize(&bin_config_struct)?;
+    
+        connection.writer.send(Message::binary(bin_config)).await.expect("error sending config!");
+    
+        Ok(())
+    }
+
+    pub async fn new(url: &str, config: cpal::SupportedStreamConfig) -> Result<Connection, Box<dyn std::error::Error>> {
+        let (mut connection, _response) = Connection::connect_to_server(url, config).await?;
+
+        Connection::send_client_config(&mut connection).await?;
+
+        Ok(connection)
+    }
+}
+
+struct AudioLink {
+    stream: cpal::Stream,
+    reader: SplitStream<WebSocketStream<MaybeTlsStream<TcpStream>>>
+}
+
+impl AudioLink {
+    async fn create_remote_audio_link(connection: Connection, input_device: &cpal::Device) -> Result<AudioLink, Box<dyn std::error::Error>> {
+        let Connection { url, mut writer, reader, config } = connection;
+    
+        let stream = input_device
+            .build_input_stream(&config.into(), move |data: &[f32], _| {
+                let bytes = data.iter().flat_map(|f| f.to_be_bytes()).collect::<Vec<u8>>();
+    
+                let runtime = tokio::runtime::Runtime::new().expect("Unable to create a runtime");
+    
+                runtime.block_on(async {
+                    writer.send(Message::binary(bytes)).await.expect("couldnt send message!");
+                });
+           
+            }, |err| eprintln!("audio stream error: {}", err),
+            None)
+            .unwrap();
+    
+        return Ok(AudioLink {
+            stream,
+            reader
+        });
+    }
+
+    pub async fn new(connection: Connection, input_device: &cpal::Device) -> Result<AudioLink, Box<dyn std::error::Error>> {
+        return AudioLink::create_remote_audio_link(connection, input_device).await;
+    }
+}
+
+
+
+async fn obtain_input_device() -> Result<(cpal::SupportedStreamConfig, cpal::Device), Box<dyn std::error::Error>> {
+    let host = cpal::default_host();
+    let input_device = host.default_input_device().expect("couldn't find default input device!");
+    let config = input_device.default_input_config()?;
+
+    return Ok((config, input_device));
+}
+
+
+
+
+
+
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    
+    // first get config of this device
+    let (config, input_device) = obtain_input_device().await?;
+
+    // then get connection
     let url = "ws://127.0.0.1:8000";
-    let (raw_socket, _) = connect_async(url).await.expect("Failed to connect");
-    
-    let (mut writer, mut reader) = raw_socket.split();
+    let connection = Connection::new(url, config).await?;
 
-    let host = cpal::default_host();
-    let input_device = host.default_input_device().expect("Failed to get default input device");
-    let config = input_device.default_input_config().expect("Failed to get default input config");
-    
-    let bin_config_struct = remoteio::BinStreamConfig {
-        channels:  config.channels(),
-        sample_rate: config.sample_rate().0,
-        buffer_size: 4096
+    // create stream where every float is send to the server
+    let mut audio_link = AudioLink::new(connection, &input_device).await?;
 
-    };
+    audio_link.stream.play().unwrap();
 
-    let bin_config = bincode::serialize(&bin_config_struct)?;
-
-    writer.send(Message::binary(bin_config)).await.expect("error sending config!");
-
-    let stream = input_device
-        .build_input_stream(&config.into(), move |data: &[f32], _| {
-            let bytes = data.iter().flat_map(|f| f.to_be_bytes()).collect::<Vec<u8>>();
-
-            let runtime = tokio::runtime::Runtime::new().expect("Unable to create a runtime");
-
-            runtime.block_on(async {
-                writer.send(Message::binary(bytes)).await.expect("couldnt send message!");
-            });
-       
-        }, |err| eprintln!("audio stream error: {}", err),
-        None)
-        .unwrap();
-
-    stream.play().unwrap();
-
-    while let Some(Ok(msg)) = reader.next().await {
+    while let Some(Ok(msg)) = audio_link.reader.next().await {
         println!("Received message: {}", msg);
     }
 
--- src/server.rs
+++ src/server.rs
@@ -1,45 +1,197 @@
 use std::convert::TryInto;
 use std::sync::atomic::AtomicBool;
-use std::sync::{Arc, Mutex};
+use std::sync::{Arc};
+use tokio::sync::Mutex;
+use tokio::net::TcpStream;
+
+
+use async_trait::async_trait;
+
 
 use bytes::{Buf, Bytes};
+use cpal::StreamConfig;
 use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
 use tokio::net::TcpListener;
+use tokio_tungstenite::WebSocketStream;
 use tokio_tungstenite::{accept_async, tungstenite::Message};
 
 use futures::{FutureExt, StreamExt};
 
+fn spawn_audio_stream(config: StreamConfig, mut audio_data_stream: (impl AsyncAudioStream + std::marker::Sync + std::marker::Send + 'static), output_device: cpal::Device) {
+
+    tokio::task::spawn_blocking(move || {
+        let runtime = tokio::runtime::Runtime::new().expect("Unable to create a runtime");
+        
+        let audio_stream = output_device
+        .build_output_stream(
+            &config,
+            move |data: &mut [f32], _: &cpal::OutputCallbackInfo| {
+
+                runtime.block_on(async {
+                    let next = audio_data_stream.get_next_batch().await.expect("couldnt get next value!");
+
+                    for (d, f) in data.into_iter().zip(next.iter()) {
+                        
+                        *d = f.clone();
+                    }
+                });
+
+
+            },
+            |err| eprintln!("audio stream error: {}", err),
+            None,
+        )
+        .unwrap();
+
+        audio_stream.play().expect("couldnt play stream");
+
+        loop {}
+    });
+}
+
+// map input -> output
+// list[]
+
+struct State {
+    virtual_devices: Vec<VirtualOutputDevice>
+}
+
+
+#[async_trait]
+trait AsyncAudioStream {
+    async fn get_next_batch(&mut self) -> Option<Vec<f32>>;
+}
+
+struct RealOutputDevice {
+    buffer: Arc<Mutex<Vec<f32>>>,
+    device: cpal::Device
+}
+
+impl RealOutputDevice {
+    fn new(device: cpal::Device, config: &cpal::StreamConfig) -> (cpal::Stream, Self) {
+        let buffer = Arc::new(Mutex::new(vec![]));
+        let buffer_rx = Arc::clone(&buffer);
+
+        let runtime = tokio::runtime::Runtime::new().expect("Unable to create a runtime");
+        
+
+        let stream = device.build_output_stream(
+            &config,
+            move |data: &mut [f32], _: &cpal::OutputCallbackInfo| {
+                runtime.block_on(async {
+                    let mut buffer = buffer_rx.lock().await;
+
+                    buffer.append(&mut data.to_vec());
+                });
+
+            },
+            |_|{},
+            None
+        ).expect("couldnt build device output stream for real output device");
+
+        stream.play().expect("couldnt play stream for real output device");
+
+
+
+        return 
+            (stream, 
+            Self {
+                buffer: buffer,
+                device: device,
+            });
+        
+    }
+}
+
+#[async_trait]
+impl AsyncAudioStream for RealOutputDevice {
+    async fn get_next_batch(&mut self) -> Option<Vec<f32>> {
+        let mut buffer = self.buffer.lock().await;
+        if buffer.len() == 0 {
+            return None;
+        }
+
+        let clone = buffer.clone();
+        buffer.clear();
+
+        return Some(clone);
+    }
+}
+struct VirtualOutputDevice {
+    buffer: Arc<Mutex<Vec<f32>>>,
+    websocket: WebSocketStream<TcpStream>
+}
+
+impl VirtualOutputDevice {
+    fn new(websocket: WebSocketStream<TcpStream>) -> Self {
+        Self {
+            buffer: Arc::new(Mutex::new(vec![])),
+            websocket: websocket
+        }
+    }
+}
+
+#[async_trait]
+impl AsyncAudioStream for VirtualOutputDevice {
+    async fn get_next_batch(&mut self) -> Option<Vec<f32>> {
+        let next_message = 
+            self.websocket
+                .next()
+                .await;
+
+        if let Some(Ok(message)) = next_message {
+
+            let mut buffer = self.buffer.lock().await;
+            
+            let mut message_data = 
+                message.into_data()
+                            .chunks(4)
+                            .map(|chunk| f32::from_be_bytes(chunk.try_into().expect("couldnt turn chunk into slice")))
+                            .collect::<Vec<f32>>();
+            
+            buffer.append(&mut message_data);
+
+            buffer.reverse();
+            buffer.truncate(1024);
+            buffer.reverse();
+        }
+
+        let mut buffer = self.buffer.lock().await;
+
+        if buffer.len() == 0 {
+            println!("buffer is empty from asyncaudiostream!");
+            return None;
+        }
+
+        let clone = buffer.clone();
+        buffer.clear();
+
+        return Some(clone);
+    }
+}
 
 #[tokio::main]
 async fn main() {
+
     
     let addr = "127.0.0.1:8000";
     let listener = TcpListener::bind(addr).await.unwrap();
     println!("Listening on: {}", addr);
 
-    
-
-    
-    
-    
-    
-    
     while let Ok((stream, _)) = listener.accept().await {
 
-        let audio_data = Arc::new(Mutex::new(Vec::<u8>::new()));
-    
         tokio::spawn(async move {
             
-            let audio_data_stream = Arc::clone(&audio_data);
-    
-            
-            let mut websocket = accept_async(stream).await.unwrap();
+
+            let mut virtual_device = VirtualOutputDevice::new(accept_async(stream).await.unwrap());
+
+
             println!(
                 "New WebSocket connection: {}",
-                websocket.get_ref().peer_addr().unwrap()
+                virtual_device.websocket.get_ref().peer_addr().unwrap()
             );
 
-            let config_message = websocket.next().await.expect("couldnt get config message!").expect("couldnt get config message!");
+            let config_message = virtual_device.websocket.next().await.expect("couldnt get config message!").expect("couldnt get config message!");
             let config_data = config_message.into_data();
 
             let config_des: remoteio::BinStreamConfig = bincode::deserialize(&config_data).expect("couldnt deserialize message!");
@@ -49,55 +201,13 @@ async fn main() {
                 buffer_size: cpal::BufferSize::Default,
             };
 
-            tokio::task::spawn_blocking(move || {
-        
-                let host = cpal::default_host();
-                let output_device = host
-                    .default_output_device()
-                    .expect("Failed to get default output device");
-
-        
-                
-                let audio_stream = output_device
-                .build_output_stream(
-                    &config,
-                    move |data: &mut [f32], _: &cpal::OutputCallbackInfo| {
-                        let mut audio_data = audio_data_stream.lock().unwrap();
-                        audio_data
-                            .chunks(4)
-                            .map(|chunk| {
-                                f32::from_be_bytes(
-                                    chunk.try_into().expect("couldn't turn u8 slice into f32"),
-                                )
-                            })
-                            .collect::<Vec<f32>>()
-                            .iter()
-                            .zip(data.iter_mut())
-                            .for_each(|(f, d)| {
-                                *d = f.clone();
-                            });
-        
-                        audio_data.clear();
-        
-                    },
-                    |err| eprintln!("audio stream error: {}", err),
-                    None,
-                )
-                .unwrap();
-        
-                audio_stream.play().expect("couldnt play stream");
-
-                loop {}
-            });
-
-            while let Some(Ok(message)) = websocket.next().await {
-                if let Message::Binary(bytes) = message {
-                    let mut audio_data_lock = audio_data.lock().unwrap();
-                    audio_data_lock.extend_from_slice(&bytes);
-                }
-            }
-
+            let host = cpal::default_host();
+            let output_device = host
+                .default_output_device()
+                .expect("Failed to get default output device");
             
+            spawn_audio_stream(config, virtual_device, output_device);
+
         });
     }
 }
